dbb2f07555b71a82970d88467b970b9930a840ce
zoltalar <voytechp@gmail.com>
2023-09-15 10:43:52 +0200

    css tables

--- pages/subscribers.vue
+++ pages/subscribers.vue
@@ -29,6 +29,56 @@
                                 </form>
                             </div>
                         </div>
+                        <div class="table-responsive">
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th width="10%">
+                                            <a href="/companies" class="sortable">ID</a>
+                                        </th>
+                                        <th width="35%">
+                                            <a href="/companies" class="sortable">Name</a>
+                                        </th>
+                                        <th width="15%">
+                                            <a href="/companies" class="sortable">Active</a>
+                                        </th>
+                                        <th class="text-end">Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td> 1 </td>
+                                        <td> Flu Demo 2023 </td>
+                                        <td><span class="badge text-bg-primary"> Yes </span></td>
+                                        <td class="text-end">
+                                            <div class="btn-group btn-group-sm">
+                                                <button title="Edit" type="button" class="btn btn-light"><i class="mdi mdi-pencil"></i></button>
+                                                <button title="Reorder Fields" type="button" class="btn btn-light"><i class="mdi mdi-sort"></i></button>
+                                                <button title="Duplicate" type="button" class="btn btn-light"><i class="mdi mdi-content-duplicate"></i></button>
+                                                <button title="View" type="button" class="btn btn-light"><i class="mdi mdi-eye-outline"></i></button>
+                                                <button title="Archive" type="button" class="btn btn-warning"><i class="mdi mdi-floppy"></i></button>
+                                                <button title="Delete" type="button" class="btn btn-danger"><i class="mdi mdi-close"></i></button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td> 2 </td>
+                                        <td> Abbott 2023 </td>
+                                        <td><span class="badge text-bg-primary"> Yes </span></td>
+                                        <td class="text-end">
+                                            <div class="btn-group btn-group-sm">
+                                                <button title="Edit" type="button" class="btn btn-light"><i class="mdi mdi-pencil"></i></button>
+                                                <button title="Reorder Fields" type="button" class="btn btn-light"><i class="mdi mdi-sort"></i></button>
+                                                <button title="Duplicate" type="button" class="btn btn-light"><i class="mdi mdi-content-duplicate"></i></button>
+                                                <button title="View" type="button" class="btn btn-light"><i class="mdi mdi-eye-outline"></i></button>
+                                                <button title="Archive" type="button" class="btn btn-warning"><i class="mdi mdi-floppy"></i></button>
+                                                <button title="Delete" type="button" class="btn btn-danger"><i class="mdi mdi-close"></i></button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>      
                     </div>
                 </div>
             </div>
