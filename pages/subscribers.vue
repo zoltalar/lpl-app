bdc18286b081e132ac55229240ba9c01c0906715
zoltalar <voytechp@gmail.com>
2023-09-15 09:40:06 +0200

    building templates

--- pages/subscribers.vue
+++ pages/subscribers.vue
@@ -1,7 +1,37 @@
 <template>
     <div>
-        <div class="page-title-box">
-            <h4 class="page-title">{{ $t('subscribers') }}</h4>
+        <div class="row">
+            <div class="col-12">
+                <div class="page-title-box">
+                    <h4 class="page-title">{{ $t('subscribers') }}</h4>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row toolbar">
+                            <div class="col-sm-8">
+                                <div class="btn-group" role="group" aria-label="Subscriber options">
+                                    <button type="button" class="btn btn-primary">{{ $t('create') }}</button>
+                                    <button type="button" class="btn btn-secondary">{{ $t('refresh') }}</button>
+                                </div>
+                            </div>
+                            <div class="col-sm-4">
+                                <form>
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" placeholder="Search">
+                                        <button type="button" class="btn btn-secondary">
+                                            <i class="mdi mdi-text-search" />
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
