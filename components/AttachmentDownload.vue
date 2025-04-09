6d7a1f5814e92f5abdcf0b3fc3b8a3ca7b53791d
zoltalar <voytechp@gmail.com>
2025-04-09 14:48:57 +0200

    attachment download prototyping

---
+++ components/AttachmentDownload.vue
@@ -0,0 +1,17 @@
+<template>
+  <a href="/attachments/download" @click.prevent="download">
+    <slot />
+  </a>
+</template>
+<script setup lang="ts">
+import type { IAttachment } from '@/types'
+// Vars
+interface Props {
+  attachment?: IAttachment | null
+}
+const props = defineProps<Props>()
+// Functions
+const download = async (): Promise<void> => {
+  const timestamp = (new Date()).getTime()
+}
+</script>
