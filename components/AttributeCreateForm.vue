fecdf0602dc0dc91219ac1098fc1df632df92e07
zoltalar <voytechp@gmail.com>
2025-04-11 16:04:05 +0200

    create attribute form - prototyping

--- components/AttributeCreateForm.vue
+++ components/AttributeCreateForm.vue
@@ -26,7 +26,13 @@
         <div class="form-group">
           <label :for="inputId('input-type')" class="form-label">{{ $t('input_type') }}</label>
           <required-input />
-          <select class="form-select" :id="inputId('input-type')" :aria-label="$t('select_input_type')" v-model="form.input_type">
+          <select
+            class="form-select"
+            :class="{'is-invalid': error('input_type') !== null}"
+            :id="inputId('input-type')"
+            :aria-label="$t('select_input_type')"
+            v-model="form.input_type"
+          >
             <option :value="null"></option>
             <option :value="type" v-for="(name, type) in inputTypes">{{ name }}</option>
           </select>
@@ -43,7 +49,14 @@
             {{ languageName(label.code) }}
           </label>
           <required-input />
-          <input type="text" class="form-control" :id="inputId('label-' + label.code)" maxlength="255" v-model="labels[i].label" />
+          <input
+            type="text"
+            class="form-control"
+            :class="{'is-invalid': error('labels.' + i + '.label') !== null}"
+            :id="inputId('label-' + label.code)"
+            maxlength="255"
+            v-model="labels[i].label"
+          />
           <div class="invalid-feedback d-block" v-if="error('labels.' + i + '.label') !== null">
             {{ error('labels.' + i + '.label') }}
           </div>
@@ -52,11 +65,106 @@
       <div class="col-lg-6">
         <div class="form-group">
           <label :for="inputId('maxlength')" class="form-label">{{ $t('max_length') }}</label>
-          <input type="number" class="form-control" :id="inputId('maxlength')" min="1" max="32767" v-model="form.maxlength" />
+          <input
+            type="number"
+            class="form-control"
+            :class="{'is-invalid': error('maxlength') !== null}"
+            :id="inputId('maxlength')"
+            min="1"
+            max="32767"
+            v-model="form.maxlength"
+          />
           <div class="invalid-feedback d-block" v-if="error('maxlength') !== null">
             {{ error('maxlength') }}
           </div>
           <div class="form-text" v-html="$t('messages.form_text_attribute_maxlength')" v-else></div>
+        </div>
+      </div>
+      <div class="col-lg-6" v-for="(placeholder, i) in placeholders">
+        <div class="form-group">
+          <label :for="inputId('placeholder-' + placeholder.code)" class="form-label">
+            {{ $t('placeholder') }}
+            -
+            {{ languageName(placeholder.code) }}
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            :class="{'is-invalid': error('placeholders.' + i + '.placeholder') !== null}"
+            :id="inputId('placeholder-' + placeholder.code)"
+            maxlength="255"
+            v-model="placeholders[i].placeholder"
+          />
+          <div class="invalid-feedback d-block" v-if="error('placeholders.' + i + '.placeholder') !== null">
+            {{ error('placeholders.' + i + '.placeholder') }}
+          </div>
+          <div class="form-text" v-html="$t('messages.form_text_attribute_placeholder')"></div>
+        </div>        
+      </div>
+      <div class="col-lg-12" v-for="(option, i) in options">
+        <div class="form-group">
+          <label :for="inputId('option-' + option.code)" class="form-label">
+            {{ $t('options') }}
+            -
+            {{ languageName(option.code) }}
+          </label>
+          <required-input v-if="optionable" />
+          <textarea
+            class="form-control"
+            :class="{'is-invalid': error('options.' + i + '.option') !== null}"
+            :id="inputId('option-' + option.code)"
+            rows="5"
+            v-model="options[i].option"></textarea>
+          <div class="invalid-feedback d-block" v-if="error('options.' + i + '.option') !== null">
+            {{ error('options.' + i + '.option') }}
+          </div>
+          <div class="form-text" v-html="$t('messages.form_text_attribute_option')" v-else></div>
+        </div>        
+      </div>
+      <div class="col-lg-6" v-for="(text, i) in inputTexts">
+        <div class="form-group">
+          <label :for="inputId('input-text-' + text.code)" class="form-label">
+            {{ $t('input_help_text') }}
+            -
+            {{ languageName(text.code) }}
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            :class="{'is-invalid': error('input_texts.' + i + '.text') !== null}"
+            :id="inputId('input-text-' + text.code)"
+            maxlength="255"
+            v-model="inputTexts[i].text" 
+          />
+          <div class="invalid-feedback d-block" v-if="error('input_texts.' + i + '.text') !== null">
+            {{ error('input_texts.' + i + '.text') }}
+          </div>
+        </div>        
+      </div>
+      <div class="col-lg-12">
+        <div class="form-group mb-0">
+          <div class="form-check form-switch">
+            <input
+              type="checkbox"
+              :id="inputId('required')"
+              class="form-check-input"
+              :true-value="1"
+              :false-value="0"
+              v-model="form.required"
+            >
+            <label :for="inputId('required')" class="form-check-label">{{ $t('required') }}</label>
+          </div>
+          <div class="form-check form-switch">
+            <input
+              type="checkbox"
+              :id="inputId('active')"
+              class="form-check-input"
+              :true-value="1"
+              :false-value="0"
+              v-model="form.active"
+            >
+            <label :for="inputId('active')" class="form-check-label">{{ $t('active') }}</label>
+          </div>
         </div>
       </div>
     </div>
@@ -65,7 +173,10 @@
 <script setup lang="ts">
 import type {
   IAttribute,
+  IAttributeInputText,
   IAttributeLabel,
+  IAttributeOption,
+  IAttributePlaceholder,
   ILanguage
 } from '@/types'
 // Vars
@@ -79,6 +190,9 @@ const fields = {
 }
 const form: Partial<IAttribute> = reactive({...fields})
 const labels = ref<IAttributeLabel[]>([])
+const placeholders = ref<IAttributePlaceholder[]>([])
+const options = ref<IAttributeOption[]>([])
+const inputTexts = ref<IAttributeInputText[]>([])
 // Composables
 const {
   errors,
@@ -90,11 +204,18 @@ const {
 const { inputTypes } = useAttribute()
 const { languages, name: languageName } = useLanguage()
 const { $_ } = useNuxtApp()
+// Computer
+const optionable = computed<boolean>(() => {
+  return [4,5].includes(Number(form.input_type))
+})
 // Watch
 watch(languages, () => {
   if (languages.value) {
     languages.value.forEach((language: ILanguage) => {
       labels.value.push({ code: language.code, label: '' })
+      placeholders.value.push({ code: language.code, placeholder: '' })
+      options.value.push({ code: language.code, option: '' })
+      inputTexts.value.push({ code: language.code, text: '' })
     })
   }
 }, { immediate: true })
@@ -106,6 +227,30 @@ const normalize = (): FormData => {
       formData.append(key, value)
     }
   })
+  if (labels.value.length > 0) {
+    labels.value.forEach((label: IAttributeLabel, i) => {
+      formData.append('labels[' + i + '][code]', label.code)
+      formData.append('labels[' + i + '][label]', label.label)
+    })
+  }
+  if (placeholders.value.length > 0) {
+    placeholders.value.forEach((placeholder: IAttributePlaceholder, i) => {
+      formData.append('placeholders[' + i + '][code]', placeholder.code)
+      formData.append('placeholders[' + i + '][placeholder]', placeholder.placeholder)
+    })
+  }
+  if (options.value.length > 0) {
+    options.value.forEach((option: IAttributeOption, i) => {
+      formData.append('options[' + i + '][code]', option.code)
+      formData.append('options[' + i + '][option]', option.option)
+    })
+  }
+  if (inputTexts.value.length > 0) {
+    inputTexts.value.forEach((text: IAttributeInputText, i) => {
+      formData.append('input_texts[' + i + '][code]', text.code)
+      formData.append('input_texts[' + i + '][text]', text.text)
+    })
+  }
   return formData
 }
 const store = async () => {
